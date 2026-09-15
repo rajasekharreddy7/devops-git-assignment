ac138963fb7c9c7fe31659858da31f375c2665d7
raj <raj@gmail.com>
2026-09-15 11:52:47 +0530

    Update application version to 1.1

--- ecommerce-app/src/main/java/com/ecommerce/App.java
+++ ecommerce-app/src/main/java/com/ecommerce/App.java
@@ -3,7 +3,7 @@ package com.ecommerce;
 public class App {
     public static void main(String[] args) {
         System.out.println("E-Commerce Application Started");
-        System.out.println("Application Version: 1.0");
+        System.out.println("Application Version: 1.1");
         System.out.println("Environment: DEV");
     }
 }
