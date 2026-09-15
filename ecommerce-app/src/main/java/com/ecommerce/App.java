ad736a7c8ad244a3e9406e3cf4e611b97e5f7747
raj <raj@gmail.com>
2026-09-15 11:00:01 +0530

    Add Maven ecommerce application

---
+++ ecommerce-app/src/main/java/com/ecommerce/App.java
@@ -0,0 +1,9 @@
+package com.ecommerce;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("E-Commerce Application Started");
+        System.out.println("Application Version: 1.0");
+        System.out.println("Environment: DEV");
+    }
+}
