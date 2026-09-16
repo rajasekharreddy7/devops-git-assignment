6c5e9e97cab51741326ddb21bd63fc2212740610
raj <raj@gmail.com>
2026-09-16 11:24:54 +0530

    Test Jenkins GitHub webhook

--- ecommerce-app/src/main/java/com/ecommerce/App.java
+++ ecommerce-app/src/main/java/com/ecommerce/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("E-Commerce Application Started");
         System.out.println("Application Version: 1.1");
-        System.out.println("Environment: DEV");
+        System.out.println("Environment: DEV - Webhook Test");
     }
 }
