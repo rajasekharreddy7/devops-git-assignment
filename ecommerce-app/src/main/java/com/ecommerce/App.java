3f28fdd1c3c5220d454614b2bdffbe0c862e52f3
raj <raj@gmail.com>
2026-09-18 09:52:28 +0530

    Convert app to HTTP service and add Kubernetes deployment

--- ecommerce-app/src/main/java/com/ecommerce/App.java
+++ ecommerce-app/src/main/java/com/ecommerce/App.java
@@ -1,9 +1,32 @@
 package com.ecommerce;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+
+        server.createContext("/", exchange -> {
+            String response =
+                    "E-Commerce Application\n" +
+                    "Version: 1.1\n" +
+                    "Environment: DEV - Kubernetes\n";
+
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+
+        server.start();
+
         System.out.println("E-Commerce Application Started");
-        System.out.println("Application Version: 1.1");
-        System.out.println("Environment: DEV - Webhook Test");
+        System.out.println("Server listening on port 8081");
     }
 }
